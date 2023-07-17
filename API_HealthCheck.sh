e327bae014401d2527c4cf354ffdb28022b790af
sharan-AWSDEV <127824859+sharan-AWSDEV@users.noreply.github.com>
2023-07-17 19:26:15 +0530

    Add files via upload

--- API_HealthCheck.sh
+++ API_HealthCheck.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-
 #Get IP address as input parameter
-
 ip_address="$1"
-
 endpoints="
   https://${ip_address}/auth/internal/v1/health?details=true
   https://${ip_address}/kwikanalytics/internal/v1/health?details=true
@@ -14,9 +11,7 @@ endpoints="
   https://${ip_address}/kwik24repman/internal/v1/health?details=true
   https://${ip_address}/kwik24stackerws/internal/v1/health?details=true
 "
-
 #Validate IP address parameter as input
-
 if [ -z "$ip_address" ]; then
   echo "Please provide an IP address as an argument."
   exit 1
@@ -24,16 +19,12 @@ fi
 
 for endpoint in $endpoints; do
   response=$(curl -sSL "$endpoint")
-  
-  #To check response of endpoint links
-
-  if echo "$response" | grep -q "status\":\"UP\""; then
+    #To check response of endpoint links
+    if echo "$response" | grep -q "status\":\"UP\""; then
     echo "Service at $endpoint is healthy."
-    
-       
+          
   else
     echo "Service at $endpoint is not healthy."
-    
-      
+         
   fi
 done
