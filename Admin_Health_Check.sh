55c62c2c98b3a7f8579c57fbfc36f7775c3ba7d3
sharan-AWSDEV <127824859+sharan-AWSDEV@users.noreply.github.com>
2023-07-18 13:14:48 +0530

    Add files via upload

--- Admin_Health_Check.sh
+++ Admin_Health_Check.sh
@@ -2,15 +2,12 @@
 # Get IP address as input parameter
 ip_address="$1"
 endpoint="https://instant-admin.bigbasket.com/internal/v1/health?details=true"
-
 # Validate IP address parameter as input
 if [ -z "$ip_address" ]; then
   echo "Please provide an IP address as an parameter."
   exit 1
 fi
-
 response=$(curl -sSL "$endpoint")
-
 # To check response of the endpoint
 if echo "$response" | grep -q "status\":\"UP\""; then
   echo "Service at $endpoint is healthy."
